b5c8f36a3d5d7950577fb31823381319498d6cb4
mmM9897 <3407653944@qq.com>
2026-09-04 12:58:31 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// 通过 contextBridge 安全暴露 IPC 接口给渲染进程
-contextBridge.exposeInMainWorld("electronAPI", {
-  // 触发时政文章抓取更新
-  refreshArticles: () => ipcRenderer.invoke("refresh-articles"),
-  // 检查是否在 Electron 环境中
-  isElectron: true,
-});
